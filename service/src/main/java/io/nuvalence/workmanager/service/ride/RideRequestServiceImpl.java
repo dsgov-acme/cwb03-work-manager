2cd05f95caea056026749c13188e0ae52324fbbb
Michael Anderson <manderson@nuvalence.io>
2024-03-01 15:44:55 -0400

    Logging
    (cherry picked from commit 28952fcdf260cc043face64c18955828452d9d96)

--- service/src/main/java/io/nuvalence/workmanager/service/ride/RideRequestServiceImpl.java
+++ service/src/main/java/io/nuvalence/workmanager/service/ride/RideRequestServiceImpl.java
@@ -82,7 +82,7 @@ public class RideRequestServiceImpl implements RideRequestService {
                             + response.getStatusCode());
         }
 
-        log.info("Successfully promise times for request: {}", request);
+        log.info("Successfully retrieved promise times for request: {}", request);
         return Optional.ofNullable(response.getBody());
     }
 
@@ -110,7 +110,7 @@ public class RideRequestServiceImpl implements RideRequestService {
                     httpClient.exchange(
                             url, HttpMethod.PUT, payload, ReservationDetailsResponse.class);
         } catch (HttpClientErrorException e) {
-            log.error("Failed to retrieve promise times for request {}", request, e);
+            log.error("Failed to submit reservation for request {}", request, e);
             if (NOT_FOUND == e.getStatusCode()) {
                 return Optional.empty();
             }
@@ -118,7 +118,7 @@ public class RideRequestServiceImpl implements RideRequestService {
         }
         if (!response.getStatusCode().is2xxSuccessful()) {
             throw new ApiException(
-                    "Failed to retrieve promise times for request.  Status code: "
+                    "Failed to submit reservation  for request.  Status code: "
                             + response.getStatusCode());
         }
 
@@ -153,7 +153,7 @@ public class RideRequestServiceImpl implements RideRequestService {
                     httpClient.exchange(
                             url, HttpMethod.GET, payload, ReservationDetailsResponse.class);
         } catch (HttpClientErrorException e) {
-            log.error("Failed to retrieve promise times for request {}", request, e);
+            log.error("Failed to retrieve reservation details for request {}", request, e);
             if (NOT_FOUND == e.getStatusCode()) {
                 return Optional.empty();
             }
@@ -161,7 +161,7 @@ public class RideRequestServiceImpl implements RideRequestService {
         }
         if (!response.getStatusCode().is2xxSuccessful()) {
             throw new ApiException(
-                    "Failed to retrieve promise times for request.  Status code: "
+                    "Failed to retrieve reservation details for request.  Status code: "
                             + response.getStatusCode());
         }
 
@@ -180,7 +180,7 @@ public class RideRequestServiceImpl implements RideRequestService {
             UUID savedLocationId = UUID.fromString(request.getDropPlaceId());
             request.setDropPlaceId(lookupPlaceIdFromSavedLocation(savedLocationId));
         } catch (IllegalArgumentException e) {
-            // id is not in UUID format - continue
+            log.info("Promise Time Request drop placeId already converted. Continuing.");
         } catch (Exception e) {
             log.error("Unexpected error when preparing request to retrieve promise times", e);
         }
@@ -189,7 +189,7 @@ public class RideRequestServiceImpl implements RideRequestService {
             UUID savedLocationId = UUID.fromString(request.getPickupPlaceId());
             request.setPickupPlaceId(lookupPlaceIdFromSavedLocation(savedLocationId));
         } catch (IllegalArgumentException e) {
-            // id is not in UUID format - continue
+            log.info("Promise Time Request pickup placeId already converted. Continuing.");
         } catch (Exception e) {
             log.error("Unexpected error when preparing request to retrieve promise times", e);
         }
