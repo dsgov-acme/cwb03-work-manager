8a2267f41b6b3c3378eae5bc278e31b9a00b3907
Michael Anderson <manderson@nuvalence.io>
2024-03-08 13:38:24 -0400

    Remove saved location interface, remove authorization for ride api

--- service/src/main/java/io/nuvalence/workmanager/service/ride/SavedLocationService.java
+++ service/src/main/java/io/nuvalence/workmanager/service/ride/SavedLocationService.java
@@ -1,16 +1,53 @@
 package io.nuvalence.workmanager.service.ride;
 
+import io.nuvalence.workmanager.service.domain.dynamicschema.DynamicEntity;
+import io.nuvalence.workmanager.service.domain.transaction.Transaction;
+import io.nuvalence.workmanager.service.mapper.RideMapper;
+import io.nuvalence.workmanager.service.repository.TransactionRepository;
 import io.nuvalence.workmanager.service.ride.models.MTALocation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public interface SavedLocationService {
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SavedLocationService {
+    private static final String SAVED_LOCATION_KEY = "MTALocation";
+    private static final String SAVED_LOCATION_STATUS = "Completed";
+    private final TransactionRepository transactionRepository;
+    private final RideMapper rideMapper;
 
-    List<MTALocation> getSavedLocationsByRiderId(String riderId);
+    public List<MTALocation> getSavedLocationsByRiderId(String riderId) {
+        List<Transaction> transactions =
+                transactionRepository.findByTransactionDefinitionKeyAndExternalIdAndStatus(
+                        SAVED_LOCATION_KEY, riderId, SAVED_LOCATION_STATUS);
+        return transactions.stream().map(this::convertToMTALocation).collect(Collectors.toList());
+    }
 
-    List<MTALocation> getSavedLocationsByUserId(String userId);
+    public List<MTALocation> getSavedLocationsByUserId(String userId) {
+        List<Transaction> transactions =
+                transactionRepository.findByTransactionDefinitionKeyAndSubjectUserIdAndStatus(
+                        SAVED_LOCATION_KEY, userId, SAVED_LOCATION_STATUS);
+        return transactions.stream().map(this::convertToMTALocation).collect(Collectors.toList());
+    }
 
-    Optional<MTALocation> getSavedLocationsById(UUID savedLocationId);
+    public Optional<MTALocation> getSavedLocationsById(UUID savedLocationId) {
+        return transactionRepository.findById(savedLocationId).map(this::convertToMTALocation);
+    }
+
+    private MTALocation convertToMTALocation(Transaction transaction) {
+
+        DynamicEntity dynamicEntity = transaction.getData();
+        String id = transaction.getId().toString();
+        MTALocation location = rideMapper.mapEntityToMTALocation(dynamicEntity);
+        location.setId(id);
+
+        return location;
+    }
 }
