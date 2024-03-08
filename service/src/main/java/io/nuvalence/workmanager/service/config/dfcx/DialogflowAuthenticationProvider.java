5d78a405a973ee820af4c34e5a08fa277f7998e7
gcastro12 <gcastro@nuvalence.io>
2024-03-08 14:44:06 -0600

    Move dfcx creds to application.yaml

--- service/src/main/java/io/nuvalence/workmanager/service/config/dfcx/DialogflowAuthenticationProvider.java
+++ service/src/main/java/io/nuvalence/workmanager/service/config/dfcx/DialogflowAuthenticationProvider.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 public class DialogflowAuthenticationProvider implements AuthenticationProvider {
-    private static String USERNAME = "DFCX_USER";
-    private static String PASSWORD = "yNCmEqJfPjxVAkRTrpHR";
+    private final String userName;
+    private final String password;
     private final String namespace;
 
-    public DialogflowAuthenticationProvider(String namespace) {
+    public DialogflowAuthenticationProvider(String userName, String password, String namespace) {
+        this.userName = userName;
+        this.password = password;
         this.namespace = namespace;
     }
 
@@ -22,7 +24,7 @@ public class DialogflowAuthenticationProvider implements AuthenticationProvider 
 
         // "validate" username and password here
         // In the future, we want something more solid than this in place for DFCX => WM auth
-        if (!username.equals(USERNAME) || !password.equals(PASSWORD)) {
+        if (!username.equals(this.userName) || !password.equals(this.password)) {
             throw new AuthenticationServiceException("Error verifying basic auth.");
         }
 
