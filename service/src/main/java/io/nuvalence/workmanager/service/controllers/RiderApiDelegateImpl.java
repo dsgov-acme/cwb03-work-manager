67e0790496aee62c24aec990e4373749383bdb90
gcastro12 <gcastro@nuvalence.io>
2024-03-08 14:03:01 -0600

    Cleanup

--- service/src/main/java/io/nuvalence/workmanager/service/controllers/RiderApiDelegateImpl.java
+++ service/src/main/java/io/nuvalence/workmanager/service/controllers/RiderApiDelegateImpl.java
@@ -16,7 +16,6 @@ import io.nuvalence.workmanager.service.utils.UserUtility;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
